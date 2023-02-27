95c37baf0e274de559595cf1083e2cc4db734583
Ilya Kuzimn <ilyacool@mail.ru>
2023-02-27 13:28:16 +0700

    change main component + add new component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-root', // ниже – обязательно «`» (ОБРАТНЫЙ СЛЭШ)
+  template: ` <h1> Hello World! </h1>
+			<p> my first component <p> <div> {{title}} </div>
+      <app-mynewcomponent></app-mynewcomponent>` , 
+  styles: [`*{color: gray;} p{font-size: 5rem;}`] 
 })
+
+
 export class AppComponent {
-  title = 'Batman';
+  title = 'My test app';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MynewcomponentComponent } from './mynewcomponent/mynewcomponent.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MynewcomponentComponent
   ],
   imports: [
     BrowserModule
